7ff914e5dbc49a19df0402abb279fd2f4f7ab4d8
rizkimadya <rizkimadya88@gmail.com>
2023-12-06 23:33:12 +0800

    add url parameter from file log

--- src/pages/Log.jsx
+++ src/pages/Log.jsx
@@ -1,40 +1,41 @@
 import React, { useState, useEffect } from "react";
 import LogTable from "../component/LogTable";
-import {
-	FormControl,
-	InputLabel,
-	Select,
-	MenuItem,
-	FormHelperText,
-} from "@mui/material";
-import FileLog from "../assets/file/1693575435_migration.log";
-import FileLog2 from "../assets/file/1693576497_migration.log";
-import FileLog3 from "../assets/file/exec-sql_1694152972.log";
 
 const Log = () => {
 	const [logData, setLogData] = useState([]);
-	const [selectedLogFile, setSelectedLogFile] = useState({
-		label: "Migration Log 1",
-		value: FileLog,
-	});
-	const [availableLogFiles] = useState([
-		{ label: "Migration Log 1", value: FileLog },
-		{ label: "Migration Log 2", value: FileLog2 },
-		{ label: "Excel SQL Log", value: FileLog3 },
-	]);
+	const [selectedLogFile, setSelectedLogFile] = useState(null);
+
+	useEffect(() => {
+		// Parse seluruh query string
+		const queryString = window.location.search;
+		const urlParams = new URLSearchParams(queryString);
+
+		// Ekstrak nilai parameter logFile dari query string
+		const logFileParam = urlParams.get("logFile");
+
+		if (logFileParam) {
+			// Setel nilai parameter logFile
+			setSelectedLogFile(logFileParam);
+		} else {
+			console.error("Tidak ada parameter logFile ditemukan pada URL.");
+		}
+	}, []); // Jalankan efek ini sekali saat komponen dipasang
 
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
-				const response = await fetch(selectedLogFile.value);
-				const data = await response.text();
+				if (selectedLogFile) {
+					// Konstruksi URL untuk file log
+					const response = await fetch(selectedLogFile);
+					const data = await response.text();
 
-				const logArray = data
-					.split("\n")
-					.filter(line => line.trim() !== "")
-					.map(line => JSON.parse(line));
+					const logArray = data
+						.split("\n")
+						.filter(line => line.trim() !== "")
+						.map(line => JSON.parse(line));
 
-				setLogData(logArray);
+					setLogData(logArray);
+				}
 			} catch (error) {
 				console.error("Error fetching log data:", error);
 			}
@@ -43,41 +44,18 @@ const Log = () => {
 		fetchData();
 	}, [selectedLogFile]);
 
-	const handleLogFileChange = event => {
-		const selectedValue = event.target.value;
-		const selectedOption = availableLogFiles.find(
-			logFile => logFile.value === selectedValue
-		);
-		setSelectedLogFile(selectedOption);
-	};
-
 	const tableHeaders = logData.length > 0 ? Object.keys(logData[0]) : [];
 
 	return (
 		<div>
 			<h1 style={{ textAlign: "center" }}>Log Data</h1>
-			<FormControl sx={{ m: 1, minWidth: 300 }}>
-				<InputLabel id="log-file-select-label">Select Log File</InputLabel>
-				<Select
-					labelId="log-file-select-label"
-					id="log-file-select"
-					value={selectedLogFile.value}
-					onChange={handleLogFileChange}
-					label="Select Log File"
-				>
-					{availableLogFiles.map(logFile => (
-						<MenuItem key={logFile.value} value={logFile.value}>
-							{logFile.label}
-						</MenuItem>
-					))}
-				</Select>
-			</FormControl>
-			{logData.length > 0 && (
+			{logData.length > 0 ? (
 				<LogTable logData={logData} tableHeaders={tableHeaders} />
+			) : (
+				<p>No log data available.</p>
 			)}
 		</div>
 	);
 };
-
 
 export default Log;
